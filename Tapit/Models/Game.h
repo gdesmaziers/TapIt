3282448e592755c1f00d08ce282e5c7f51638d07
Grégory DESMAZIERS <Greg@MacBook-Pro-de-Gregory.local>
2016-01-16 16:05:05 +0100

    Create Game Model

---
+++ Tapit/Models/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Tapit
+//
+//  Created by Grégory DESMAZIERS on 16/01/2016.
+//  Copyright © 2016 Voodoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ Tapit/Models/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  Tapit
+//
+//  Created by Grégory DESMAZIERS on 16/01/2016.
+//  Copyright © 2016 Voodoo. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
